b35a19f7c73db467d9e5567b31f792d1c899f52e
rongqingliu <phpgege@gmail.com>
2022-05-03 17:18:59 +0800

    gohome

---
+++ src/utils/request/baseRequest.ts
@@ -0,0 +1,28 @@
+import { Request } from './Request'
+// import { AxiosResponse } from 'axios'
+
+import { IRequestConfig } from './ITypes'
+
+interface IBaseResponse<T> {
+  code: number
+  data: T
+  msg: string
+}
+
+// 重写返回类型
+interface IBaseRequestConfig<T, R> extends IRequestConfig<IBaseResponse<R>> {
+  data?: T
+}
+
+export const baseRequest = (instanceConfig: any) => {
+  const request = new Request(instanceConfig)
+
+  return <D = any, T = any>(config: IBaseRequestConfig<D, T>) => {
+    const { method = 'GET' } = config
+    if (['get', 'GET'].includes(method)) {
+      config.params = config.data
+    }
+    return request.request<IBaseResponse<T>>(config)
+  }
+}
+
--- src/utils/request/hmRequest.ts
+++ src/utils/request/hmRequest.ts
@@ -1,32 +1,10 @@
-import { Request } from './Request'
-import { AxiosResponse } from 'axios'
+import { baseRequest } from './baseRequest'
 
-import { IRequestConfig } from './ITypes'
-
-interface IHmResponse<T> {
-  code: number
-  data: T
-  msg: string
-}
-
-// 重写返回类型
-interface IHmRequestConfig<T, R> extends IRequestConfig<IHmResponse<R>> {
-  data?: T
-}
-
-const request = new Request({
-  baseURL: '/aa',
-  timeout: 1000 * 60 * 5, // 5分钟
+export const hmRequest = baseRequest({
+  baseURL: '/bb',
+  timeout: 1000 * 60 * 5,
   interceptors: {
-    requestInterceptors: (config) => config,
-    responseInterceptors: (result: AxiosResponse) => result,
+    // requestInterceptors: (config: any) => config,
+    // responseInterceptors: (result: AxiosResponse) => result,
   },
 })
-
-export const hmRequest = <D = any, T = any>(config: IHmRequestConfig<D, T>) => {
-  const { method = 'GET' } = config
-  if (['get', 'GET'].includes(method)) {
-    config.params = config.data
-  }
-  return request.request<IHmResponse<T>>(config)
-}
